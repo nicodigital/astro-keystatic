9d280c8b65cd4f0619bab4ea421f5dbfbe76fc3b
Nicolás González <nicolook.pro@gmail.com>
2024-10-26 16:54:57 -0300

    Add GitHub Login

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+    repo: {
+      owner: "nicodigital",
+      name: "astro-keystatic"
+    }   
   },
   collections: {
     posts: collection({
