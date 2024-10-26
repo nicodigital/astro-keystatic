2b17b1f1283cac35285b8dea4427cfb95ef47267
Nicolás González <nicolook.pro@gmail.com>
2024-10-26 16:46:54 -0300

    add Nerlify Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   output: 'hybrid',
+  adapter: netlify(),
+  prefetch: true
 });
